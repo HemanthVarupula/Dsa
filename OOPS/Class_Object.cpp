e55c07ce9e257e89eed2dd57bafd42d1d898a975
Hemanth Varupula <varupulahemanth@gmailo.com>
2025-09-18 04:11:49 +0530

    added classes ,objects, encapsulation

--- OOPS/Class_Object.cpp
+++ OOPS/Class_Object.cpp
@@ -1,25 +1,58 @@
+// #include<iostream>
+// using namespace std;
+// class Name{
+   
+//     private:
+//     int age;
+//     public: 
+//     string name; 
+//      Name(){
+//         cout<<"hello"<<endl;
+//     };
+//     int weight;
+//     Name(int w,string n){
+//         weight=w;
+//         name=n;
+//     }
+   
+
+//     void print(){
+//         cout<<weight<<endl;
+//         cout<<name<<endl;
+//     }
+//     void setAge(int nage){
+//         age=nage;
+//     }
+//     void getAge(){
+//        cout<<age;
+//     }
+// };
+// int main(){
+//     Name naam(74,"hemanth");
+//     naam.print();
+//     // naam.print("hemanth");
+//     // naam.setAge(23);
+//     // naam.getAge();
+// }
+
+
 #include<iostream>
 using namespace std;
-class Name{
-    private:
-    int age;
-    public:  
-   string name;
+class Details{
+    public:
+    Details(){
+        cout<<"printing details"<<endl;
+    }
+    string name;
+    int num;
+    void print(string name,double num){
+        this->name=name;
+        this->num=num;
+        cout<<"name "<<name <<"num is "<<num<<endl;
+    }
 
-    void print(string newName){
-        name=newName;
-        cout<<name<<endl;
-    }
-    void setAge(int nage){
-        age=nage;
-    }
-    void getAge(){
-       cout<<age;
-    }
 };
 int main(){
-    Name naam;
-    naam.print("hemanth");
-    naam.setAge(23);
-    naam.getAge();
+    Details d1;
+    d1.print("hemanth",90146);
 }
